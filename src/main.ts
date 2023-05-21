3148397c331697578f61c265eb09a3f9a2b9b97d
meitrix8208 <maurrod2001@outlook.com>
2023-05-21 14:20:39 -0500

    swagger works!

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('user proyect')
+    .setDescription('api for user proyect')
+    .setVersion('1.0')
+    .addTag('user')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // http://localhost:3000/api
   await app.listen(3000);
 }
 bootstrap();
